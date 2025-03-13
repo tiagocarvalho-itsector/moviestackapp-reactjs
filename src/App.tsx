d3c410b488ac92b421d0cdfdd1d24c29262c3b5f
tiagocarvalho-itsector <tiago.carvalho@itsector.pt>
2025-03-13 21:15:58 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+import "./App.css";
+
+function App() {
+  return (
+    <Container className="background d-flex justify-content-center align-items-center">
+      <h1 className="text-danger">Movie Stack 🎬</h1>
+    </Container>
+  );
+}
+
+export default App;
