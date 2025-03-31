c3a07a7b929fbc6ac8df284b229983c5ce0a18ba
tiagocarvalho-itsector <tiago.carvalho@itsector.pt>
2025-03-31 17:12:55 +0100

    add extra content

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import { Movies } from "./screens/components/moviesComponent";
+import { Extra } from "./screens/components/extraComponent";
+import { useState } from "react";
 
 function App() {
+  const [showExtra, setShowExtra] = useState(false);
+
+  function toggleExtra(): void {
+    setShowExtra(!showExtra);
+  }
+
   return (
-    <div className="background">
-      <Container className="d-flex flex-column align-items-center justify-content-between py-2">
-        <h1 className="movie-stack-title">Movie Stack 🎬</h1>
-        <Movies />
-      </Container>
-    </div>
+    <>
+      <Button className="mb-2" variant="outline-warning" onClick={toggleExtra}>
+        {showExtra ? "Hide" : "Show"} Extra Content
+      </Button>
+      {showExtra ? (
+        <Extra />
+      ) : (
+        <div className="background">
+          <Container className="d-flex flex-column align-items-center justify-content-between py-2">
+            <h1 className="movie-stack-title">Movie Stack 🎬</h1>
+            <Movies />
+          </Container>
+        </div>
+      )}
+    </>
   );
 }
 
--- src/screens/components/extraComponent.tsx
+++ src/screens/components/extraComponent.tsx
@@ -1,5 +1,29 @@
+import {
+  Breadcrumb,
+  Typography,
+} from "@bcp-nextgen-dx-component-factory/design-system";
 import React from "react";
 
 export const Extra: React.FC = () => {
-  return <></>;
+  return (
+    <div style={{ color: "white" }} className="p-3">
+      <Breadcrumb
+        breadcrumbs={[
+          {
+            onPress: function noRefCheck() {},
+            text: "CRÉDITOS",
+          },
+          {
+            onPress: function noRefCheck() {},
+            text: "CARTEIRA",
+          },
+          {
+            onPress: function noRefCheck() {},
+            text: "GARANTIAS E AVALES",
+          },
+        ]}
+      />
+      <Typography appearance="headline-100-03">Garantias e Avales</Typography>
+    </div>
+  );
 };
