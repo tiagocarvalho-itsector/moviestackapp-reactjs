eee3ce097d22a9480580870f8d26e84118e7d957
tiagocarvalho-itsector <tiago.carvalho@itsector.pt>
2025-03-31 16:22:49 +0100

    add new deps

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.tsx";
+import { GlobalStyle, ThemeProvider } from '@bcp-nextgen-dx-component-factory/design-system';
+import { Accolade } from '@bcp-nextgen-dx-component-factory/theme-accolade';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <ThemeProvider theme={Accolade}>
+    <GlobalStyle />
     <App />
+    </ThemeProvider>
   </StrictMode>
 );
